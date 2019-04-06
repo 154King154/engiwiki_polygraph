ef64dabc537cb59ad5ca985cfffa1dbb9406a7ca
Александр Гоптарь <King154Rus@bk.ru>
2019-04-06 22:00:36 +0700

    Update main_on_python.py
    add matplotlib and numpy

--- main/main_on_python.py
+++ main/main_on_python.py
@@ -1,8 +1,11 @@
 import serial
 import json
+import time
+import numpy as np
+import matplotlib.pyplot as plt
 
 ser = serial.Serial(
-    port='/dev/ttyACM1',
+    port='/dev/ttyACM0',
     baudrate=57600,
     parity=serial.PARITY_ODD,
     stopbits=serial.STOPBITS_TWO,
@@ -20,22 +23,55 @@ def convertData(byte_str):
     decode_str = byte_str.decode('utf-8')
     data = json.loads(decode_str[:-1])
 
-    alpha = data['alpha']
-    beta = data['beta']
-    emgi = data['emgi']
-    gsr = data['gsr']
-    temp = data['temp']
-    rr = data['rr']
+    alpha = float(data['alpha'])
+    beta = float(data['beta'])
+    emgi = float(data['emgi'])
+    gsr = float(data['gsr'])
+    temp = float(data['temp'])
+    rr = float(data['rr'])
 
 
 
 #ser.open()
-i=0
-while (i<15):
+time_i=time.time()
+time_j=time.time()
+test_gsr = []
+test_emgi = []
+while (abs(time_i-time_j)<12):
     byte_str = ser.readline()
     if (byte_str[0]==123) and (byte_str[1]==34):
         convertData(byte_str)
-        print(gsr)
-        i+=1
+        print(abs(time_i-time_j))
+        test_gsr.append(gsr)
+        test_emgi.append(emgi)
+    time_j = time.time()
+#print(np.std(test_gsr), np.mean(test_gsr))
 
-ser.close()             # close port
+
+x = list(range(len(test_gsr)))
+
+
+plt.figure(1)
+
+# gsr
+plt.subplot(221)
+plt.plot(x, test_gsr)
+#plt.yscale('gsr')
+plt.title('gsr')
+plt.grid(True)
+
+
+# emgi
+plt.subplot(222)
+plt.plot(x, test_emgi)
+#plt.yscale('emgi')
+plt.title('emgi')
+plt.grid(True)
+
+plt.show()
+
+test_gsr = np.array(test_gsr)
+test_emgi = np.array(test_emgi)
+print(np.std(test_gsr), np.std(test_emgi))
+
+ser.close()
